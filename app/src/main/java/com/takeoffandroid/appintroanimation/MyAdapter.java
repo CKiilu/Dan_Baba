dc9b4a08185024ba4894ae1cdb80b23dd485fa98
CKiilu <cndeti@gmail.com>
2016-03-17 08:31:43 +0300

    Called items in onbindviewholder

--- app/src/main/java/com/takeoffandroid/appintroanimation/MyAdapter.java
+++ app/src/main/java/com/takeoffandroid/appintroanimation/MyAdapter.java
@@ -8,14 +8,19 @@ import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import java.util.Collections;
+import java.util.List;
+
 /**
  * Created by chris on 3/17/16.
  */
 public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
     private final LayoutInflater inflater;
+    List<MyFrag4Data> data = Collections.emptyList();
 
-    public MyAdapter(Context context){
+    public MyAdapter(Context context, List<MyFrag4Data> data){
         inflater = LayoutInflater.from(context);
+        this.data = data;
     }
     @Override
     public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
@@ -26,7 +31,9 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
 
     @Override
     public void onBindViewHolder(MyViewHolder holder, int position) {
-
+        MyFrag4Data current = data.get(position);
+        holder.title.setText(current.title);
+        holder.img.setImageResource(current.img);
     }
 
     @Override
--- app/src/main/java/com/takeoffandroid/appintroanimation/MyFrag4Data.java
+++ app/src/main/java/com/takeoffandroid/appintroanimation/MyFrag4Data.java
@@ -4,6 +4,6 @@ package com.takeoffandroid.appintroanimation;
  * Created by chris on 3/17/16.
  */
 public class MyFrag4Data {
-    private  int iconId;
-    private String title;
+    int img;
+    String title;
 }
