5f97d5d2c5b8e58d1d3e44ba4699a1bbd83fc556
CKiilu <cndeti@gmail.com>
2016-03-17 08:24:45 +0300

    Added views to viewholder, instanciated objects

--- app/src/main/java/com/takeoffandroid/appintroanimation/MyAdapter.java
+++ app/src/main/java/com/takeoffandroid/appintroanimation/MyAdapter.java
@@ -1,16 +1,27 @@
 package com.takeoffandroid.appintroanimation;
 
+import android.content.Context;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 /**
  * Created by chris on 3/17/16.
  */
 public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
+    private final LayoutInflater inflater;
+
+    public MyAdapter(Context context){
+        inflater = LayoutInflater.from(context);
+    }
     @Override
     public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        return null;
+        View view  = inflater.inflate(R.layout.row_layout, parent, false);
+        MyViewHolder holder = new MyViewHolder(view);
+        return holder;
     }
 
     @Override
@@ -23,9 +34,13 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
         return 0;
     }
     class MyViewHolder extends RecyclerView.ViewHolder{
+        TextView title;
+        ImageView img;
 
         public MyViewHolder(View itemView) {
             super(itemView);
+            title = (TextView) itemView.findViewById(R.id.fragtxt);
+            img = (ImageView) itemView.findViewById(R.id.fragimg);
         }
     }
 }
